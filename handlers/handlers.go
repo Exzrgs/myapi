6fc16bfdd53f5a7a5d12c0df34bad65cae224f5f
Exzrgs <s.nakao0120@gmail.com>
2023-08-16 23:43:16 +0900

    I implemented query parameter

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -20,7 +20,27 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 
 // ブログ記事の一覧を取得
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Article List\n")
+	queryMap := req.URL.Query()
+
+	var page int
+
+	p, ok := queryMap["page"]
+
+	if ok && len(p) > 0 {
+		var err error
+
+		page, err = strconv.Atoi(p[0])
+
+		if err != nil {
+			http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+			return
+		}
+	} else {
+		page = 1
+	}
+
+	resString := fmt.Sprintf("Article List (page %d)\n", page)
+	io.WriteString(w, resString)
 }
 
 // 記事ナンバーID番の登校データを取得
