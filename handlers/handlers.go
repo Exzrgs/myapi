858e911398b40443395c4392ccc411a32b30a67e
Exzrgs <s.nakao0120@gmail.com>
2023-08-19 19:48:35 +0900

    updated handlers to respond by json

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,12 +1,11 @@
 package handlers
 
 import (
-	"fmt"
+	"encoding/json"
 	"io"
 	"net/http"
-	"strconv"
 
-	"github.com/gorilla/mux"
+	"github.com/Exzrgs/myapi/models"
 )
 
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
@@ -15,53 +14,106 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 
 // ブログ記事の投稿をする
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Article...\n")
+	// io.WriteString(w, "Posting Article...\n")
+
+	article := models.Article1
+	jsonData, err := json.Marshal(article)
+
+	if err != nil {
+		http.Error(w, "fail to encode to json\n", http.StatusInternalServerError)
+	} else {
+		w.Write(jsonData)
+	}
 }
 
 // ブログ記事の一覧を取得
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	queryMap := req.URL.Query()
+	/*
+		queryMap := req.URL.Query()
 
-	var page int
+		var page int
 
-	p, ok := queryMap["page"]
+		p, ok := queryMap["page"]
 
-	if ok && len(p) > 0 {
-		var err error
+		if ok && len(p) > 0 {
+			var err error
 
-		page, err = strconv.Atoi(p[0])
+			page, err = strconv.Atoi(p[0])
+
+			if err != nil {
+				http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+				return
+			}
+		} else {
+			page = 1
+		}
+
+		resString := fmt.Sprintf("Article List (page %d)\n", page)
+		io.WriteString(w, resString)
+	*/
+	ls := []models.Article{}
+	ls = append(ls, models.Article1, models.Article2)
+
+	jsonData, err := json.Marshal(ls)
+
+	if err != nil {
+		http.Error(w, "fail to encode to json\n", http.StatusInternalServerError)
+	} else {
+		w.Write(jsonData)
+	}
+}
+
+// 記事ナンバーID番の登校データを取得
+func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
+	/*
+		articleID, err := strconv.Atoi(mux.Vars(req)["id"])
 
 		if err != nil {
 			http.Error(w, "Invalid query parameter", http.StatusBadRequest)
 			return
 		}
-	} else {
-		page = 1
-	}
 
-	resString := fmt.Sprintf("Article List (page %d)\n", page)
-	io.WriteString(w, resString)
-}
+		resString := fmt.Sprintf("Article No.%d\n", articleID)
+		io.WriteString(w, resString)
+	*/
 
-// 記事ナンバーID番の登校データを取得
-func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
+	article := models.Article1
+
+	jsonData, err := json.Marshal(article)
 
 	if err != nil {
-		http.Error(w, "Invalid query parameter", http.StatusBadRequest)
-		return
+		http.Error(w, "fail to encode to json\n", http.StatusInternalServerError)
+	} else {
+		w.Write(jsonData)
 	}
-
-	resString := fmt.Sprintf("Article No.%d\n", articleID)
-	io.WriteString(w, resString)
 }
 
 // 記事にいいねをつける
 func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Nice...\n")
+	// io.WriteString(w, "Posting Nice...\n")
+
+	article := models.Article1
+
+	jsonData, err := json.Marshal(article)
+
+	if err != nil {
+		http.Error(w, "fail to encode to json\n", http.StatusInternalServerError)
+	} else {
+		w.Write(jsonData)
+	}
 }
 
 // 記事にコメントをつける
 func CommentHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Comment...\n")
+	// io.WriteString(w, "Posting Comment...\n")
+
+	comment := models.Comment1
+
+	jsonData, err := json.Marshal(comment)
+
+	if err != nil {
+		http.Error(w, "fail to encode to json\n", http.StatusInternalServerError)
+	} else {
+		w.Write(jsonData)
+	}
 }
